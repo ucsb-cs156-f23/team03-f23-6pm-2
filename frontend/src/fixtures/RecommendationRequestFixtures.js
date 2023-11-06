15c10a9772f24611590f45905693143f44ee1664
Wei Jung Huang <whuang602@csilvm-13.cs.ucsb.edu>
2023-11-05 16:22:28 -0800

    wh - implemented rec request fixtures

---
+++ frontend/src/fixtures/RecommendationRequestFixtures.js
@@ -0,0 +1,43 @@
+const RecommendationRequstFixtures = {
+    oneRequest: {
+        "id": 1,
+        "requesterEmail": "ballen@gmail.com",
+        "professorEmail": "thomas@gmail.com",
+        "explanation": "grad school",
+        "dateRequested": "2023-10-06T00:18:23.630Z",
+        "dateNeeded": "2023-11-06T00:18:23.630Z",
+        "done": false
+    },
+    ThreeRequests: [
+        {
+            "id": 1,
+            "requesterEmail": "tony@gmail.com",
+            "professorEmail": "ricky@gmail.com",
+            "explanation": "BS/MS program",
+            "dateRequested": "2023-03-06T00:18:23.630Z",
+            "dateNeeded": "2023-06-25T00:18:23.630Z",
+            "done": true
+        },
+        {
+            "id": 2,
+            "requesterEmail": "michael@gmail.com",
+            "professorEmail": "paul@gmail.com",
+            "explanation": "help me please",
+            "dateRequested": "2022-02-06T00:18:23.630Z",
+            "dateNeeded": "2024-12-05T00:18:23.630Z",
+            "done": false
+        },
+        {
+            "id": 3,
+            "requesterEmail": "davis@gmail.com",
+            "professorEmail": "ouil@gmail.com",
+            "explanation": "REU position at UCSD",
+            "dateRequested": "2023-05-06T00:18:23.630Z",
+            "dateNeeded": "2023-11-06T00:18:23.630Z",
+            "done": true
+        }
+    ]
+};
+
+
+export { ucsbDatesFixtures };
