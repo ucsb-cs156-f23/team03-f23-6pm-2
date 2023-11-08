36e3da46e92e274944a34b71c1f305cd811836d4
justinchung712 <justinchung712@gmail.com>
2023-11-08 00:46:00 -0800

    Add js fixtures for MenuItemReview

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,39 @@
+const menuItemReviewFixtures = {
+    oneMenuItemReview: {
+        "id": 1,
+        "itemId": 1,
+        "reviewerEmail": "reviewer1@ucsb.edu",
+        "stars": 1,
+        "dateReviewed": "2023-01-02T12:00:00",
+        "comments": "review1"
+    },
+    threeMenuItemReviews: [
+        {
+            "id": 2,
+            "itemId": 2,
+            "reviewerEmail": "reviewer2@ucsb.edu",
+            "stars": 2,
+            "dateReviewed": "2023-03-04T12:00:00",
+            "comments": "review2"
+        },
+        {
+            "id": 3,
+            "itemId": 3,
+            "reviewerEmail": "reviewer3@ucsb.edu",
+            "stars": 3,
+            "dateReviewed": "2023-05-06T12:00:00",
+            "comments": "review3"
+        },
+        {
+            "id": 4,
+            "itemId": 4,
+            "reviewerEmail": "reviewer4@ucsb.edu",
+            "stars": 4,
+            "dateReviewed": "2023-07-08T12:00:00",
+            "comments": "review4"
+        }
+    ]
+};
+
+
+export { menuItemReviewFixtures };
