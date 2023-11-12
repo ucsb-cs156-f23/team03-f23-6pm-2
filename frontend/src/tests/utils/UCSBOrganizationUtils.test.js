ea293c9b3a9c90534e1281d3768f06b06c22db58
cstraw2021 <86491289+cstraw2021@users.noreply.github.com>
2023-11-11 20:10:40 -0800

    Update UCSBOrganizationUtils.test.js

--- frontend/src/tests/utils/UCSBOrganizationUtils.test.js
+++ frontend/src/tests/utils/UCSBOrganizationUtils.test.js
@@ -11,7 +11,7 @@ jest.mock('react-toastify', () => {
     };
 });
 
-describe("ucsbOrganizationUtils", () => {
+describe("UCSBOrganizationUtils", () => {
 
     describe("onDeleteSuccess", () => {
 
