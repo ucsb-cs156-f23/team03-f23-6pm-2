64f94ea064fd1e751e1f5dab214bfec5f0b9d98a
whuang602 <72839223+whuang602@users.noreply.github.com>
2023-11-14 00:24:32 -0800

    wh - implemented rec request table and tests

---
+++ frontend/src/stories/components/RecommendationRequest/RecommendationRequestTable.stories.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import RecommendationRequestTable from "main/components/RecommendationRequest/RecommendationRequestTable";
+import { RecommendationRequestFixtures } from 'fixtures/RecommendationRequestFixtures';
+import { currentUserFixtures } from 'fixtures/currentUserFixtures';
+import { rest } from "msw";
+
+export default {
+    title: 'components/RecommendationRequest/RecommendationRequestTable',
+    component: RecommendationRequestTable
+};
+
+const Template = (args) => {
+    return (
+        <RecommendationRequestTable {...args} />
+    )
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+    requests: []
+};
+
+export const ThreeItemsOrdinaryUser = Template.bind({});
+
+ThreeItemsOrdinaryUser.args = {
+    requests: RecommendationRequestFixtures.ThreeRequests,
+    currentUser: currentUserFixtures.userOnly,
+};
+
+export const ThreeItemsAdminUser = Template.bind({});
+ThreeItemsAdminUser.args = {
+    requests: RecommendationRequestFixtures.ThreeRequests,
+    currentUser: currentUserFixtures.adminUser,
+}
+
+ThreeItemsAdminUser.parameters = {
+    msw: [
+        rest.delete('/api/recommendationrequests', (req, res, ctx) => {
+            window.alert("DELETE: " + JSON.stringify(req.url));
+            return res(ctx.status(200),ctx.json({}));
+        }),
+    ]
+};
+
